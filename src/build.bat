f5a3da67a15695d5b39785c0fedfa72f4aa5b0de
Adam Sheridan <adam@asheridan.co.uk>
2012-12-03 05:06:21 +0000

    added sublime project, sass & build scripts

---
+++ src/build.bat
@@ -0,0 +1,2 @@
+call sass src/sass/main.scss public/css/main.css
+echo "Build Completed -- OK"
---
+++ src/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Compiling SASS..."
+sass src/sass/main.scss public/css/main.css
+echo "OK."
+
+echo "DONE."
